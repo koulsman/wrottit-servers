d7bc502f07f6cc46997223dac95266f0b086b365
Stavros Koulas <koulas@verusplus.com>
2025-07-06 13:40:44 +0300

    users => user server

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Import routes (from the old UserServer/PostServer/CommunityServer)
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/UserServer');
 const postRoutes = require('./routes/PostServer');
 const communityRoutes = require('./routes/CommunityServer');
 // const communityRoutes = require('./routes/communities');
 
 // Mount routes
-app.use('/users', userRoutes);
+app.use('/UserServer', userRoutes);
 app.use('/PostServer', postRoutes);
 app.use('/CommunityServer', communityRoutes);
 // app.use('/communities', communityRoutes);
