9c1c93d721c561b6252d819ed90f0f6485c19198
Stavros Koulas <koulas@verusplus.com>
2025-07-28 16:11:42 +0300

    fix in user schema

--- UserModel.js
+++ UserModel.js
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema({
   liked: {type: Array,required: false },
   commented: {type: Array, required: false},
   posts: {type: Array, required: false},
-   communitiesJoined: {type: Array, default: [],required: false, unique: true},
-  saved: {type: Array, default: [],required: false, unique: true},
+   communitiesJoined: {type: Array, default: [],required: false},
+  saved: {type: Array, default: [],required: false},
 }, { 
   collection: 'users' // Specify the collection name explicitly
 });
